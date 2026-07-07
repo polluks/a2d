polluks/a2d

commit c68fc27186075c1ec4ea5fde0c7c9c77c08ec040

DeskTop: Make DragSelect a child proc of HandleClick
Just internal reorganization to make dependencies clearer.

No functional changes.

src/desktop/main.s
@@ -507,7 +507,7 @@ offset_table:
         jmp     _IconClick
       END_IF
 
-        TAIL_CALL DragSelect, A=#0
+        TAIL_CALL _DragSelect, A=#0
     END_IF
 
     IF A = #MGTK::Area::menubar ; menu?
@@ -609,7 +609,7 @@ window_click:
 
         ;; Not an icon - maybe a drag?
         jsr     _ActivateClickedWindow ; no-op if already active
-        TAIL_CALL DragSelect, A=active_window_id
+        TAIL_CALL _DragSelect, A=active_window_id
     END_IF
 
         ;; --------------------------------------------------
@@ -1000,6 +1000,192 @@ prev_selected_icon:
     END_IF
         rts
 .endproc ; _MaybeUpdateDropTargetFromName
+
+;;; --------------------------------------------------
+;;; Drag Selection
+;;; Inputs: A = window_id (0 for desktop)
+;;; Assert: `cached_window_id` == A
+
+.proc _DragSelect
+
+PARAM_BLOCK, $10
+window_id       .byte    ; 0 = desktop, assumed to be active otherwise
+delta           .tag    MGTK::Point
+initial_pos     .tag    MGTK::Point
+last_pos        .tag    MGTK::Point
+END_PARAM_BLOCK
+
+        sta     window_id
+        jsr     CacheWindowIconList
+
+        lda     window_id
+    IF NOT_ZERO
+        ;; Map initial event coordinates
+        jsr     PrepActiveWindowScreenMapping
+        jsr     _CoordsScreenToWindow
+    END_IF
+
+        ;; Stash initial coords
+        COPY_STRUCT MGTK::Point, event_params::coords, initial_pos
+
+        ;; Is this actually a drag?
+        jsr     PeekEvent
+        lda     event_params::kind
+    IF A <> #MGTK::EventKind::drag
+        ;; No, just a click; optionally clear selection
+        lda     MainLoop::modifiers
+        and     #kExtendSelectionModifierMask
+        jeq     ClearSelection  ; don't clear if mis-clicking
+        rts
+    END_IF
+
+        ;; --------------------------------------------------
+        ;; Prep selection
+        lda     window_id
+        cmp     selected_window_id
+        bne     clear
+
+        lda     MainLoop::modifiers
+        and     #kExtendSelectionModifierMask
+        bne     :+
+
+clear:  jsr     ClearSelection
+        CALL    CacheWindowIconList, A=window_id
+:
+
+        ;; --------------------------------------------------
+        ;; Set up drawing port, draw initial rect
+        lda     window_id
+    IF NOT_ZERO
+        jsr     UnsafeSetPortForWindowIdAndAdjustForEntries ; ASSERT: not obscured
+    ELSE
+        jsr     InitSetDesktopPort
+    END_IF
+
+        ;; Any `ClearSelection` calls above may modify `tmp_rect`
+        ldx     #.sizeof(MGTK::Point)-1
+    DO
+        lda     initial_pos,x
+        sta     tmp_rect::topleft,x
+        sta     tmp_rect::bottomright,x
+    WHILE dex : POS
+
+        jsr     FrameTmpRect
+
+        ;; --------------------------------------------------
+        ;; Event loop
+event_loop:
+
+        ;; Done the drag?
+        jsr     PeekEvent
+        lda     event_params::kind
+    IF A <> #MGTK::EventKind::drag
+
+        jsr     FrameTmpRect
+
+        jsr     CachedIconsScreenToWindow
+
+        ;; Process all icons in window
+        ldx     #0              ; X = index
+      DO
+       IF X = cached_window_icon_count
+        jmp     CachedIconsWindowToScreen
+       END_IF
+
+        txa
+        pha                     ; A = index
+
+        ;; Check if icon should be selected
+        copy8   cached_window_icon_list,x, icon_param
+        ITK_CALL IconTK::IconInRect, icon_param
+       IF NOT ZERO
+
+        ;; Already selected?
+        CALL    IsIconSelected, A=icon_param
+        IF NE
+        ;; Highlight and add to selection
+        ;; NOTE: Does not use `AddIconToSelection` because we perform
+        ;; a more optimized drawing below.
+        ITK_CALL IconTK::HighlightIcon, icon_param
+        CALL    AddToSelectionList, A=icon_param
+        copy8   window_id, selected_window_id
+        ELSE
+        ;; Unhighlight and remove from selection
+        ITK_CALL IconTK::UnhighlightIcon, icon_param
+        CALL    RemoveFromSelectionList, A=icon_param
+        END_IF
+
+        lda     window_id
+        IF ZERO
+        ITK_CALL IconTK::DrawIcon, icon_param
+        ELSE
+        ITK_CALL IconTK::DrawIconRaw, icon_param ; CHECKED (drag select)
+        END_IF
+       ELSE
+        jsr     CheckEvents
+       END_IF
+
+        pla                     ; A = index
+        tax                     ; X = index
+      WHILE inx : NOT_ZERO      ; always
+    END_IF
+
+        ;; --------------------------------------------------
+        ;; Check movement threshold
+        lda     window_id
+    IF NOT_ZERO
+        jsr     _CoordsScreenToWindow
+    END_IF
+
+        ldx     #2              ; loop over dimensions
+    DO
+        sub16   event_params::coords,x, last_pos,x, delta,x
+
+        lda     delta+1,x
+      IF NEG
+        lda     delta,x         ; negate
+        eor     #$FF
+        sta     delta,x
+        inc     delta,x
+      END_IF
+
+        ;; TODO: Experiment with making this lower.
+        kDragBoundThreshold = 5
+
+        lda     delta,x
+        cmp     #kDragBoundThreshold
+        bcs     beyond
+
+    WHILE dex : dex : POS       ; next dimension
+        jmp     event_loop
+
+        ;; Beyond threshold; erase rect
+beyond:
+        jsr     FrameTmpRect
+
+        COPY_STRUCT event_params::coords, last_pos
+
+        ;; --------------------------------------------------
+        ;; Figure out coords for rect's left/top/bottom/right
+
+        ldx     #2              ; loop over dimensions
+    DO
+      IF scmp16 event_params::coords,x, initial_pos,x : NEG
+        copy16  event_params::coords,x, tmp_rect::topleft,x
+        copy16  initial_pos,x, tmp_rect::bottomright,x
+      ELSE
+        copy16  initial_pos,x, tmp_rect::topleft,x
+        copy16  event_params::coords,x, tmp_rect::bottomright,x
+      END_IF
+    WHILE dex : dex : POS       ; next dimension
+
+        jsr     FrameTmpRect
+        jmp     event_loop
+
+.proc _CoordsScreenToWindow
+        TAIL_CALL MapCoordsScreenToWindow, AX=#event_params::coords
+.endproc ; _CoordsScreenToWindow
+.endproc ; _DragSelect
 
 ;;; --------------------------------------------------
 
@@ -5493,192 +5679,6 @@ exception_flag:
         ;; Create icons and draw contents
         jmp     RefreshView
 .endproc ; ActivateAndRefreshWindow
-
-;;; ============================================================
-;;; Drag Selection
-;;; Inputs: A = window_id (0 for desktop)
-;;; Assert: `cached_window_id` == A
-
-.proc DragSelect
-
-PARAM_BLOCK, $10
-window_id       .byte    ; 0 = desktop, assumed to be active otherwise
-delta           .tag    MGTK::Point
-initial_pos     .tag    MGTK::Point
-last_pos        .tag    MGTK::Point
-END_PARAM_BLOCK
-
-        sta     window_id
-        jsr     CacheWindowIconList
-
-        lda     window_id
-    IF NOT_ZERO
-        ;; Map initial event coordinates
-        jsr     PrepActiveWindowScreenMapping
-        jsr     _CoordsScreenToWindow
-    END_IF
-
-        ;; Stash initial coords
-        COPY_STRUCT MGTK::Point, event_params::coords, initial_pos
-
-        ;; Is this actually a drag?
-        jsr     PeekEvent
-        lda     event_params::kind
-    IF A <> #MGTK::EventKind::drag
-        ;; No, just a click; optionally clear selection
-        lda     MainLoop::modifiers
-        and     #kExtendSelectionModifierMask
-        jeq     ClearSelection  ; don't clear if mis-clicking
-        rts
-    END_IF
-
-        ;; --------------------------------------------------
-        ;; Prep selection
-        lda     window_id
-        cmp     selected_window_id
-        bne     clear
-
-        lda     MainLoop::modifiers
-        and     #kExtendSelectionModifierMask
-        bne     :+
-
-clear:  jsr     ClearSelection
-        CALL    CacheWindowIconList, A=window_id
-:
-
-        ;; --------------------------------------------------
-        ;; Set up drawing port, draw initial rect
-        lda     window_id
-    IF NOT_ZERO
-        jsr     UnsafeSetPortForWindowIdAndAdjustForEntries ; ASSERT: not obscured
-    ELSE
-        jsr     InitSetDesktopPort
-    END_IF
-
-        ;; Any `ClearSelection` calls above may modify `tmp_rect`
-        ldx     #.sizeof(MGTK::Point)-1
-    DO
-        lda     initial_pos,x
-        sta     tmp_rect::topleft,x
-        sta     tmp_rect::bottomright,x
-    WHILE dex : POS
-
-        jsr     FrameTmpRect
-
-        ;; --------------------------------------------------
-        ;; Event loop
-event_loop:
-
-        ;; Done the drag?
-        jsr     PeekEvent
-        lda     event_params::kind
-    IF A <> #MGTK::EventKind::drag
-
-        jsr     FrameTmpRect
-
-        jsr     CachedIconsScreenToWindow
-
-        ;; Process all icons in window
-        ldx     #0              ; X = index
-      DO
-       IF X = cached_window_icon_count
-        jmp     CachedIconsWindowToScreen
-       END_IF
-
-        txa
-        pha                     ; A = index
-
-        ;; Check if icon should be selected
-        copy8   cached_window_icon_list,x, icon_param
-        ITK_CALL IconTK::IconInRect, icon_param
-       IF NOT ZERO
-
-        ;; Already selected?
-        CALL    IsIconSelected, A=icon_param
-        IF NE
-        ;; Highlight and add to selection
-        ;; NOTE: Does not use `AddIconToSelection` because we perform
-        ;; a more optimized drawing below.
-        ITK_CALL IconTK::HighlightIcon, icon_param
-        CALL    AddToSelectionList, A=icon_param
-        copy8   window_id, selected_window_id
-        ELSE
-        ;; Unhighlight and remove from selection
-        ITK_CALL IconTK::UnhighlightIcon, icon_param
-        CALL    RemoveFromSelectionList, A=icon_param
-        END_IF
-
-        lda     window_id
-        IF ZERO
-        ITK_CALL IconTK::DrawIcon, icon_param
-        ELSE
-        ITK_CALL IconTK::DrawIconRaw, icon_param ; CHECKED (drag select)
-        END_IF
-       ELSE
-        jsr     CheckEvents
-       END_IF
-
-        pla                     ; A = index
-        tax                     ; X = index
-      WHILE inx : NOT_ZERO      ; always
-    END_IF
-
-        ;; --------------------------------------------------
-        ;; Check movement threshold
-        lda     window_id
-    IF NOT_ZERO
-        jsr     _CoordsScreenToWindow
-    END_IF
-
-        ldx     #2              ; loop over dimensions
-    DO
-        sub16   event_params::coords,x, last_pos,x, delta,x
-
-        lda     delta+1,x
-      IF NEG
-        lda     delta,x         ; negate
-        eor     #$FF
-        sta     delta,x
-        inc     delta,x
-      END_IF
-
-        ;; TODO: Experiment with making this lower.
-        kDragBoundThreshold = 5
-
-        lda     delta,x
-        cmp     #kDragBoundThreshold
-        bcs     beyond
-
-    WHILE dex : dex : POS       ; next dimension
-        jmp     event_loop
-
-        ;; Beyond threshold; erase rect
-beyond:
-        jsr     FrameTmpRect
-
-        COPY_STRUCT event_params::coords, last_pos
-
-        ;; --------------------------------------------------
-        ;; Figure out coords for rect's left/top/bottom/right
-
-        ldx     #2              ; loop over dimensions
-    DO
-      IF scmp16 event_params::coords,x, initial_pos,x : NEG
-        copy16  event_params::coords,x, tmp_rect::topleft,x
-        copy16  initial_pos,x, tmp_rect::bottomright,x
-      ELSE
-        copy16  initial_pos,x, tmp_rect::topleft,x
-        copy16  event_params::coords,x, tmp_rect::bottomright,x
-      END_IF
-    WHILE dex : dex : POS       ; next dimension
-
-        jsr     FrameTmpRect
-        jmp     event_loop
-
-.proc _CoordsScreenToWindow
-        TAIL_CALL MapCoordsScreenToWindow, AX=#event_params::coords
-.endproc ; _CoordsScreenToWindow
-.endproc ; DragSelect
 
 ;;; ============================================================
 ;;; Initiate keyboard-based window moving

commit 3e3ff345cfb9b04a84d91878679e0d4332a9d7e3

Desktop: Fix close animation target after preview
Regressed in c68fc27 - moving things around left the
InvokeDeskAccWithIcon proc in the memory area used by DAs for large
loads, e.g. when playing music files. So when previewing a music file,
the restore would reload the proc. Unfortunately the proc used
self-modifying code to stash the icon number for the close animation
target, so this was getting reset to a dummy value each time.

Add the PROC_USED_IN_OVERLAY guard macro to the proc, and shuffle
things around to make stuff happy again.

src/desktop/main.s
@@ -1981,6 +1981,156 @@ check_header:
 .endproc ; ReadLinkFile
 
 ;;; ============================================================
+;;; Invoke a DA, with path set to first file selection
+;;; Input: `src_path_buf` has DA absolute path
+
+.proc InvokeDeskAccByPath
+        ;; * Can't use `dst_path_buf` as it is within DA_IO_BUFFER
+        ;; * Can't use `src_path_buf` as it holds file selection
+        COPY_STRING src_path_buf, tmp_path_buf ; Use this to launch the DA
+
+        copy8   #0, src_path_buf ; Signal no file selection
+
+        ;; As a convenience for DAs, pass path to first selected icon.
+        lda     selected_icon_count
+    IF NOT_ZERO
+        lda     selected_icon_list ; first selected icon
+      IF A <> trash_icon_num    ; ignore trash
+        jsr     GetIconPath     ; `operation_src_path` set to path; A=0 on success
+        jne     ShowAlert       ; `ERR_INVALID_PATHNAME`
+        CALL    CopyToSrcPath, AX=#operation_src_path
+      END_IF
+    END_IF
+        CALL    GetIconToAnimate, AX=#tmp_path_buf
+        tay
+        FALL_THROUGH_TO InvokeDeskAccWithIcon, AX=#tmp_path_buf
+.endproc ; InvokeDeskAccByPath
+
+;;; ============================================================
+;;; Invoke Desk Accessory
+;;; Input: A,X = DA pathname (relative is OK)
+;;;        Y = icon id to animate ($FF for none)
+
+        PROC_USED_IN_OVERLAY    ; Prevent DA overlay from trashing SMC
+.proc InvokeDeskAccWithIcon
+        stax    open_params::pathname
+
+        tya
+        sta     icon            ; can't use stack, as DAs can modify
+    IF NC
+        CALL    AnimateWindowOpen, X=#$FF ; desktop
+    END_IF
+
+        ;; --------------------------------------------------
+        ;; Load the DA
+
+    DO
+        MLI_CALL OPEN, open_params
+      IF CS
+        CALL    ShowAlert, A=#kErrInsertSystemDisk
+        ASSERT_EQUALS ::kAlertResultTryAgain, 0
+        REDO_IF ZERO
+        rts                     ; cancel, so fail
+      END_IF
+
+        lda     open_params::ref_num
+        sta     read_header_params::ref_num
+        sta     read_params::ref_num
+        sta     close_params::ref_num
+        MLI_CALL READ, read_header_params
+
+        lda     DAHeader::aux_length
+        ora     DAHeader::aux_length+1
+      IF NOT ZERO
+        ;; Aux memory segment
+        copy16  DAHeader::aux_length, read_params::request_count
+        MLI_CALL READ, read_params
+        copy16  #DA_LOAD_ADDRESS, STARTLO
+        copy16  #DA_LOAD_ADDRESS, DESTINATIONLO
+        add16   #DA_LOAD_ADDRESS-1, DAHeader::aux_length, ENDLO
+        CALL    AUXMOVE, C=1    ; main>aux
+      END_IF
+
+        ;; Main memory segment
+        copy16  DAHeader::main_length, read_params::request_count
+        MLI_CALL READ, read_params
+        MLI_CALL CLOSE, close_params
+    DONE
+
+        ;; --------------------------------------------------
+        ;; Invoke it
+
+        jsr     SetCursorPointer ; before invoking DA
+        jsr     DA_LOAD_ADDRESS
+
+        ;; --------------------------------------------------
+        ;; Restore state
+
+        jsr     InitSetDesktopPort ; DA's port destroyed, set something real as current
+        jsr     ShowClockForceUpdate
+        jsr     ClearUpdates    ; following DA close (just in case)
+
+        icon := *+1
+        lda     #SELF_MODIFIED_BYTE
+    IF NC
+        CALL    AnimateWindowClose, X=#$FF ; desktop
+    END_IF
+
+        jmp     SetCursorPointer ; after invoking DA
+
+.params DAHeader
+aux_length:     .word   0
+main_length:    .word   0
+.endparams
+
+        DEFINE_OPEN_PARAMS open_params, 0, DA_IO_BUFFER
+        DEFINE_READWRITE_PARAMS read_header_params, DAHeader, .sizeof(DAHeader)
+        DEFINE_READWRITE_PARAMS read_params, DA_LOAD_ADDRESS, kDAMaxSize
+        DEFINE_CLOSE_PARAMS close_params
+
+.endproc ; InvokeDeskAccWithIcon
+
+;;; ============================================================
+;;; Copy the string at $06 to target at A,X
+;;; Inputs: Source string at $06, target buffer at A,X
+;;; Output: String length in A
+
+        PROC_USED_IN_FORMAT_ERASE_OVERLAY
+        PROC_USED_IN_OVERLAY
+.proc CopyPtr1ToBuf
+        ptr1 := $06
+
+        stax    addr
+        ldy     #0
+        lda     (ptr1),y
+        tay
+    DO
+        lda     (ptr1),y
+        addr := *+1
+        sta     SELF_MODIFIED,y
+    WHILE dey : POS
+        rts
+.endproc ; CopyPtr1ToBuf
+
+;;; Copy the string at $08 to target at A,X
+;;; Inputs: Source string at $08, target buffer at A,X
+;;; Output: String length in A
+.proc CopyPtr2ToBuf
+        ptr2 := $08
+
+        stax    addr
+        ldy     #0
+        lda     (ptr2),y
+        tay
+    DO
+        lda     (ptr2),y
+        addr := *+1
+        sta     SELF_MODIFIED,y
+    WHILE dey : POS
+        rts
+.endproc ; CopyPtr2ToBuf
+
+;;; ============================================================
 ;;; Uppercase a string
 ;;; Input: A,X = Address
 ;;; Trashes $06
@@ -2342,46 +2492,6 @@ entry_num:
 .endproc ; InvokeSelectorEntry
 
 ;;; ============================================================
-;;; Copy the string at $06 to target at A,X
-;;; Inputs: Source string at $06, target buffer at A,X
-;;; Output: String length in A
-
-        PROC_USED_IN_FORMAT_ERASE_OVERLAY
-        PROC_USED_IN_OVERLAY
-.proc CopyPtr1ToBuf
-        ptr1 := $06
-
-        stax    addr
-        ldy     #0
-        lda     (ptr1),y
-        tay
-    DO
-        lda     (ptr1),y
-        addr := *+1
-        sta     SELF_MODIFIED,y
-    WHILE dey : POS
-        rts
-.endproc ; CopyPtr1ToBuf
-
-;;; Copy the string at $08 to target at A,X
-;;; Inputs: Source string at $08, target buffer at A,X
-;;; Output: String length in A
-.proc CopyPtr2ToBuf
-        ptr2 := $08
-
-        stax    addr
-        ldy     #0
-        lda     (ptr2),y
-        tay
-    DO
-        lda     (ptr2),y
-        addr := *+1
-        sta     SELF_MODIFIED,y
-    WHILE dey : POS
-        rts
-.endproc ; CopyPtr2ToBuf
-
-;;; ============================================================
 
 CmdAbout := AboutDialogProc
 
@@ -2441,115 +2551,6 @@ skip:   iny
         jmp     LaunchFileByPathOnSystemDisk
 .endproc ; CmdDeskAccImpl
 CmdDeskAcc      := CmdDeskAccImpl::start
-
-;;; ============================================================
-;;; Invoke a DA, with path set to first file selection
-;;; Input: `src_path_buf` has DA absolute path
-
-.proc InvokeDeskAccByPath
-        ;; * Can't use `dst_path_buf` as it is within DA_IO_BUFFER
-        ;; * Can't use `src_path_buf` as it holds file selection
-        COPY_STRING src_path_buf, tmp_path_buf ; Use this to launch the DA
-
-        copy8   #0, src_path_buf ; Signal no file selection
-
-        ;; As a convenience for DAs, pass path to first selected icon.
-        lda     selected_icon_count
-    IF NOT_ZERO
-        lda     selected_icon_list ; first selected icon
-      IF A <> trash_icon_num    ; ignore trash
-        jsr     GetIconPath     ; `operation_src_path` set to path; A=0 on success
-        jne     ShowAlert       ; `ERR_INVALID_PATHNAME`
-        CALL    CopyToSrcPath, AX=#operation_src_path
-      END_IF
-    END_IF
-        CALL    GetIconToAnimate, AX=#tmp_path_buf
-        tay
-        FALL_THROUGH_TO InvokeDeskAccWithIcon, AX=#tmp_path_buf
-.endproc ; InvokeDeskAccByPath
-
-;;; ============================================================
-;;; Invoke Desk Accessory
-;;; Input: A,X = DA pathname (relative is OK)
-;;;        Y = icon id to animate ($FF for none)
-
-.proc InvokeDeskAccWithIcon
-        stax    open_params::pathname
-
-        tya
-        sta     icon            ; can't use stack, as DAs can modify
-    IF NC
-        CALL    AnimateWindowOpen, X=#$FF ; desktop
-    END_IF
-
-        ;; --------------------------------------------------
-        ;; Load the DA
-
-    DO
-        MLI_CALL OPEN, open_params
-      IF CS
-        CALL    ShowAlert, A=#kErrInsertSystemDisk
-        ASSERT_EQUALS ::kAlertResultTryAgain, 0
-        REDO_IF ZERO
-        rts                     ; cancel, so fail
-      END_IF
-
-        lda     open_params::ref_num
-        sta     read_header_params::ref_num
-        sta     read_params::ref_num
-        sta     close_params::ref_num
-        MLI_CALL READ, read_header_params
-
-        lda     DAHeader::aux_length
-        ora     DAHeader::aux_length+1
-      IF NOT ZERO
-        ;; Aux memory segment
-        copy16  DAHeader::aux_length, read_params::request_count
-        MLI_CALL READ, read_params
-        copy16  #DA_LOAD_ADDRESS, STARTLO
-        copy16  #DA_LOAD_ADDRESS, DESTINATIONLO
-        add16   #DA_LOAD_ADDRESS-1, DAHeader::aux_length, ENDLO
-        CALL    AUXMOVE, C=1    ; main>aux
-      END_IF
-
-        ;; Main memory segment
-        copy16  DAHeader::main_length, read_params::request_count
-        MLI_CALL READ, read_params
-        MLI_CALL CLOSE, close_params
-    DONE
-
-        ;; --------------------------------------------------
-        ;; Invoke it
-
-        jsr     SetCursorPointer ; before invoking DA
-        jsr     DA_LOAD_ADDRESS
-
-        ;; --------------------------------------------------
-        ;; Restore state
-
-        jsr     InitSetDesktopPort ; DA's port destroyed, set something real as current
-        jsr     ShowClockForceUpdate
-        jsr     ClearUpdates    ; following DA close (just in case)
-
-        icon := *+1
-        lda     #SELF_MODIFIED_BYTE
-    IF NC
-        CALL    AnimateWindowClose, X=#$FF ; desktop
-    END_IF
-
-        jmp     SetCursorPointer ; after invoking DA
-
-.params DAHeader
-aux_length:     .word   0
-main_length:    .word   0
-.endparams
-
-        DEFINE_OPEN_PARAMS open_params, 0, DA_IO_BUFFER
-        DEFINE_READWRITE_PARAMS read_header_params, DAHeader, .sizeof(DAHeader)
-        DEFINE_READWRITE_PARAMS read_params, DA_LOAD_ADDRESS, kDAMaxSize
-        DEFINE_CLOSE_PARAMS close_params
-
-.endproc ; InvokeDeskAccWithIcon
 
 ;;; ============================================================
 

tests/desktop/animation.lua
@@ -54,3 +54,18 @@ test.Step(
     a2dtest.MultiSnap(15, "window animates closed")
     emu.wait(5)
 end)
+
+test.Step(
+  "Ensure animation ends on correct icon after preview",
+  function()
+    a2d.OpenPath("/A2.DESKTOP/SAMPLE.MEDIA/JESU.JOY", {no_validate=true})
+
+    util.WaitFor(
+      "player showing", function()
+        return a2dtest.OCRScreen():match("Electric Duet")
+    end)
+
+    apple2.EscapeKey()
+    a2dtest.MultiSnap(60, "should animate back to JESU.JOY icon")
+    emu.wait(5)
+end)