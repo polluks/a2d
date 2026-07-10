polluks/a2d

commit 7c864d85b66e811c11a67dad3a0282b6e1c34bd2

MGTK: Don't invoke first menu item without shortcuts on ^@
Control+Shift+2 a.k.a. ^@ yields the ASCII NUL value. This was used in
the menu definitions to signal "no shortcut". MGTK didn't special-case
this, so the first menu item without a shortcut would be invoked when
this key combination was pressed.

In DeskTop this was "About Apple II DeskTop" so the dialog would show!

Address this by having the menu defining macros put a value with the
high bit set; MGTK strips the high bit from the keyboard input so it
can never match. This allows MGTK apps to actually define a shortcut
with ^@ if they want to.

src/desktop/auxmem.s
@@ -155,7 +155,7 @@ label_show_link:
         DEFINE_MENU_ITEM label_close_all
         DEFINE_MENU_SEPARATOR
         DEFINE_MENU_ITEM label_get_info, res_char_menu_item_get_info_shortcut
-        DEFINE_MENU_ITEM_NOMOD label_rename_icon, CHAR_RETURN, CHAR_RETURN
+        DEFINE_MENU_ITEM_NOMOD label_rename_icon, CHAR_RETURN
         DEFINE_MENU_ITEM label_duplicate_icon, res_char_menu_item_duplicate_shortcut
         DEFINE_MENU_SEPARATOR
         DEFINE_MENU_ITEM label_copy_selection

tests/menus.lua
@@ -286,5 +286,14 @@ test.Step(
     a2d.OpenPath("/A2.DESKTOP")
     a2d.OpenMenu(a2d.FILE_MENU)
     test.Snap("verify New Folder, Close, Close All are enabled")
+    apple2.EscapeKey()
 end)
 
+
+test.Step(
+  "Control-Shift-2 doesn't show first menu item without shortcut",
+  function()
+    apple2.ControlKey("@")
+    a2d.WaitForRepaint()
+    test.Snap("verify About dialog not showing")
+end)

src/mgtk/mgtk.inc
@@ -324,6 +324,11 @@ SetKeyEvent     = $2E           ; If set, keypresses are ignored by Tool Kit
 ;;; --------------------------------------------------
 ;;; Menu Manager Calls
 
+;;; Since 0 is a valid ASCII code (Ctrl+Shift+2 produces ^@ a.k.a.
+;;; NUL), an arbitrary value with the high bit set should be used to
+;;; signify "no shortuct".
+no_shortcut = $FF
+
 ;;; Menu Bar record:
 ;;;
 ;;; .word       count           Number of menu bar items
@@ -347,8 +352,8 @@ SetKeyEvent     = $2E           ; If set, keypresses are ignored by Tool Kit
 .struct MenuItem
         options         .byte ; bit 0=OA, 1=SA, 2=mark, 5=check, 6=filler, 7=disabled
         mark_char       .byte ; Custom mark character if mark option set
-        char1           .byte ; ASCII code of shortcut #1 (e.g. uppercase B); or 0
-        char2           .byte ; ASCII code of shortcut #2 (e.g. lowercase b, or same); or 0
+        char1           .byte ; ASCII code of shortcut #1 (e.g. uppercase B); or no_shortcut
+        char2           .byte ; ASCII code of shortcut #2 (e.g. lowercase b, or same); or no_shortcut
         name            .addr ; Address of length-prefixed string
 .endstruct
 
@@ -883,30 +888,30 @@ ycoord: .word   AS_WORD(top)
 .endmacro
 
 ;;; For menu items with open-apple modifier shortcuts, or no shortcuts
-;;; NOTE: If shortcut is A-Z, char2 is to_lower(char1); else char2=char1
+;;; NOTE: If shortcut is A-Z, char2 is to_lower(char1)
 .macro DEFINE_MENU_ITEM saddr, shortcut
-        .if .paramcount > 1
+    .ifnblank shortcut
 
         .byte   MGTK::MenuOpt::open_apple|MGTK::MenuOpt::solid_apple ; option byte
         .byte   0               ; mark_char
         .byte   shortcut        ; char1
 
-        .if kBuildSupportsLowercase && 'A' <= shortcut && shortcut <= 'Z'
+      .if kBuildSupportsLowercase && 'A' <= shortcut && shortcut <= 'Z'
         .byte   TO_LOWER(shortcut) ; char2
-        .else
-        .byte   shortcut
-        .endif
+      .else
+        .byte   MGTK::no_shortcut
+      .endif
         .addr   saddr           ; item_str_pointer
 
-        .else
+    .else
 
         .byte   0               ; option byte
         .byte   0               ; mark_char
-        .byte   0               ; char1
-        .byte   0               ; char2
+        .byte   MGTK::no_shortcut ; char1
+        .byte   MGTK::no_shortcut ; char2
         .addr   saddr           ; item_str_pointer
 
-        .endif
+    .endif
 .endmacro
 
 ;;; For menu items with control keys (e.g. CHAR_RETURN), requiring no modifiers
@@ -914,7 +919,11 @@ ycoord: .word   AS_WORD(top)
         .byte   0               ; option byte
         .byte   0               ; mark_char
         .byte   shortcut1       ; char1
+    .ifnblank shortcut2
         .byte   shortcut2       ; char2
+    .else
+        .byte   MGTK::no_shortcut
+    .endif
         .addr   saddr           ; item_str_pointer
 .endmacro
 

commit 59e0b09dbd89eafec5bc7f2b358210b0a6b1dd0d

DeskTop: Correctly show ⏎ glyph for File > Rename, not ^M
Regressed in 7c864d8; MGTK has a special case to show the specified
glyph for control character shortcuts if the same char is passed
twice. That commit accidentally removed the second char, causing
the fallback behavior.

src/desktop/auxmem.s
@@ -155,7 +155,7 @@ label_show_link:
         DEFINE_MENU_ITEM label_close_all
         DEFINE_MENU_SEPARATOR
         DEFINE_MENU_ITEM label_get_info, res_char_menu_item_get_info_shortcut
-        DEFINE_MENU_ITEM_NOMOD label_rename_icon, CHAR_RETURN
+        DEFINE_MENU_ITEM_NOMOD label_rename_icon, CHAR_RETURN, CHAR_RETURN
         DEFINE_MENU_ITEM label_duplicate_icon, res_char_menu_item_duplicate_shortcut
         DEFINE_MENU_SEPARATOR
         DEFINE_MENU_ITEM label_copy_selection

tests/desktop/menus.lua
@@ -626,3 +626,12 @@ test.Step(
         a2d.WaitForRepaint()
     end)
 end)
+
+test.Step(
+  "File > Rename shortcut display",
+  function()
+    a2d.SelectPath("/A2.DESKTOP")
+    a2d.OpenMenu(a2d.FILE_MENU)
+    test.ExpectNotMatch(a2dtest.OCRScreen(), "Rename *^M", "rename shortcut should not be ^M")
+    apple2.EscapeKey()
+end)